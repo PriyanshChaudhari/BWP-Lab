95d20d7b6e5ad14c01803abb71ca0f3c183ed745
Priyansh Chaudhari <priyanshchaudhari3@gmail.com>
2023-08-26 16:54:31 +0530

    #19 cartoon animation modified

--- Lab-5/Q5/tr/jquery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $("#d1, #d2,#d31,#d32").animate({left: '+=250px',}, 2000, function(){
-            $("#d1, #d2,#d31,#d32").animate({top: '+=250px'}, 2000);
-            $("#d1, #d2,#d31,#d32").stop();
-        });
-    });
-});
